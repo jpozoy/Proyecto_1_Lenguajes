0d91c233557f85aece9ed44c302edb1086ad7c65
jeff-pozo <pozojefferson410@gmail.com>
2024-09-03 18:36:15 -0600

    Configuración git ignore

--- build/cJSON/fuzzing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jeff/lenguajes/proyecto-1/cJSON/fuzzing
-# Build directory: /home/jeff/lenguajes/proyecto-1/build/cJSON/fuzzing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
