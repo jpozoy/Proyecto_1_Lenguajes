c0059ac277a95acdfbf8213109af9e398a4b44ea
jeff-pozo <pozojefferson410@gmail.com>
2024-09-03 18:40:36 -0600

    Actualización de git ignore

---
+++ build/cJSON/fuzzing/CMakeFiles/fuzz_main.dir/cjson_read_fuzzer.c.o.d
@@ -0,0 +1,23 @@
+cJSON/fuzzing/CMakeFiles/fuzz_main.dir/cjson_read_fuzzer.c.o: \
+ /home/jeff/lenguajes/proyecto-1/cJSON/fuzzing/cjson_read_fuzzer.c \
+ /usr/include/stdc-predef.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h /usr/include/string.h \
+ /home/jeff/lenguajes/proyecto-1/cJSON/fuzzing/../cJSON.h
